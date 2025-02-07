82b3d1040a9509d3436e7dc8692f49a8f2ddda83
Mercy Bhakta <mercybhakta@gmail.com>
2025-02-07 10:57:59 -0800

    Added main.js file creation

--- s3.tf
+++ s3.tf
@@ -1,12 +1,65 @@
 # create bucket
 resource "aws_s3_bucket" "cloud_resume" {
-    bucket = var.bucket_name
-    tags = {
-      Project = "CloudResumeChallenge"
-    }
+  bucket = var.bucket_name
+  tags = {
+    Project = "CloudResumeChallenge"
+  }
 }
 
 # uploading website resources
+
+# create main.js so it uses the correct api url
+# use $$ to escape $
+resource "local_file" "visitorCounterScript" {
+  content  = <<EOF
+visitorCount = 0;
+const apiUrl = "${aws_apigatewayv2_api.http.api_endpoint}";
+const route  = "/getVisitorCount"
+
+fetch(apiUrl+route)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    visitorCount = Number(data["visitorCount"]);
+    console.log("total visitor count: ", visitorCount);
+
+    // change number suffix (eg 1st 2nd 3rd 4th 5th 6th)
+    let suffix = "th";
+    switch (visitorCount % 10) {
+      case 1:
+        if (visitorCount != 11) {
+          suffix = "st";
+        }
+        break;
+      case 2:
+        if (visitorCount != 12) {
+          suffix = "nd";
+        }
+        break;
+      case 3:
+        if (visitorCount != 13) {
+          suffix = "rd";
+        }
+        break;
+      default:
+        suffix = "th";
+    }
+
+    document.getElementById(
+      "visitorCount"
+    ).innerText = `Fun fact, you're the $${visitorCount}$${suffix} visitor to this page`;
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+  EOF
+  filename = "${path.module}/src/scripts/main.js"
+}
 
 # "[template_files] gathers all of the files under a particular base directory"
 # this makes it easier to upload a folder w/ nested folders to s3
@@ -19,13 +72,14 @@ module "template_files" {
 }
 
 resource "aws_s3_object" "static_files" {
-  bucket       = aws_s3_bucket.cloud_resume.id
+  bucket = aws_s3_bucket.cloud_resume.id
 
-  for_each = module.template_files.files
+  for_each     = module.template_files.files
   key          = each.key
   content_type = each.value.content_type
 
-  source  = each.value.source_path
-  content = each.value.content
+  source     = each.value.source_path
+  content    = each.value.content
+  depends_on = [local_file.visitorCounterScript]
 }
 
