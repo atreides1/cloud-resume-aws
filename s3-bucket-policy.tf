c31c733e90e77d600af3a6f44d79ab350b7d3b9d
Mercy Bhakta <mercybhakta@gmail.com>
2025-02-07 10:57:36 -0800

    Formatting changes

--- s3-bucket-policy.tf
+++ s3-bucket-policy.tf
@@ -3,22 +3,22 @@ resource "aws_s3_bucket_policy" "cloudfront_access" {
   bucket = aws_s3_bucket.cloud_resume.id
   policy = jsonencode({
     "Version" : "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "AllowCloudFrontServicePrincipalReadOnly",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "cloudfront.amazonaws.com"
-            },
-            "Action": "s3:GetObject",
-            "Resource": "arn:aws:s3:::${var.bucket_name}/*",
-            "Condition": {
-                "StringEquals": {
-                    "AWS:SourceArn": aws_cloudfront_distribution.cloud_resume.arn
-                }
-            }
+    "Statement" : [
+      {
+        "Sid" : "AllowCloudFrontServicePrincipalReadOnly",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudfront.amazonaws.com"
+        },
+        "Action" : "s3:GetObject",
+        "Resource" : "arn:aws:s3:::${var.bucket_name}/*",
+        "Condition" : {
+          "StringEquals" : {
+            "AWS:SourceArn" : aws_cloudfront_distribution.cloud_resume.arn
+          }
         }
+      }
     ]
   })
-    depends_on = [ aws_s3_bucket.cloud_resume, aws_cloudfront_distribution.cloud_resume ]
+  depends_on = [aws_s3_bucket.cloud_resume, aws_cloudfront_distribution.cloud_resume]
 }
