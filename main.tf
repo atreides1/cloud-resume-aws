7407578b009268a288c78b23a333d332bbbf9db2
Mercy Bhakta <mercybhakta@gmail.com>
2025-02-22 09:01:55 -0800

    Passing variables to child modules

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
 module "backend" {
-  source = "./modules/backend"
+  source              = "./modules/backend"
+  cname               = var.cname
+  domain_name         = var.domain_name
+  dynamodb_table_name = var.dynamodb_table_name
 }
 
 module "frontend" {
-  source       = "./modules/frontend"
-  api_endpoint = module.backend.api_endpoint
+  source                  = "./modules/frontend"
+  api_endpoint            = module.backend.api_endpoint
+  bucket_name             = var.bucket_name
+  cname                   = var.cname
+  domain_name             = var.domain_name
+  region                  = var.region
+  route53_hosting_zone_id = var.route53_hosting_zone_id
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,16 @@
+variable "api_endpoint" {
+  type        = string
+  description = "the API Gateway endpoint URL"
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "Name for the s3 bucket that stores the website resources"
+}
+
 variable "cname" {
   type        = string
+  default     = "resume"
   description = "the cname for the website hosting the resume (eg www)"
 }
 
@@ -8,7 +19,19 @@ variable "domain_name" {
   description = "The domain name for the cloud resume website"
 }
 
+variable "dynamodb_table_name" {
+  type        = string
+  default     = "visitor_counter"
+  description = "The table name in DynamoDB that stores the visitor count"
+}
+
 variable "region" {
   type        = string
+  default     = "us-west-2"
   description = "A valid AWS region (eg us-east-1)"
+}
+
+variable "route53_hosting_zone_id" {
+  type        = string
+  description = "The ID of the hosting zone"
 }
